593dddb5d74b298ce446862fa7fe839c55b6f37c
fateh bouretoua <bouretoua_fateh@hotmail.com>
2023-09-22 23:52:50 +0200

    Add GetUserInput files

---
+++ GetUserInput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static string GetUserInput()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write($"Hello, {name}! Please enter some text: ");
+        string text = Console.ReadLine();
+
+        return text;
+    }
+
+    static void Main()
+    {
+        string enteredText = GetUserInput();
+        Console.WriteLine($"You entered the following text: {enteredText}");
+    }
+}
+
